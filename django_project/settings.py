66c91507a4b348481a41666e0ac68b9a459c241f
SRaflian <sraflian@outlook.com>
2024-02-10 20:00:40 +0700

    setting up mysql database

--- django_project/settings.py
+++ django_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'appxco',
+        'USER': 'root',
+        'PASSWORD': 'newengland2030',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
