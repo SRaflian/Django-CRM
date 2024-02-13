bef8c2a5d208c6beeb3e8157b50b880e76394125
SRaflian <sraflian@outlook.com>
2024-02-13 19:44:24 +0700

    menambah fitur dashboard untuk melihat data dan add record

--- customers/urls.py
+++ customers/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.Record_dashboard, name='record-dashboard'),
+    path('record/<int:id>/', views.customer_record_view, name='customer_record'),
+    path('record/add/', views.add_record, name='add_record'),
 ]
 
 
--- customers/views.py
+++ customers/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Record
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import AddRecordForm
+from django.urls import reverse
+
 
 # Create your views here.
 @login_required
 def Record_dashboard(request):
     records = Record.objects.all()  # Fetch all records from the database
-    return render(request, 'users/record-dashboard.html', {'records': records})
+    return render(request, 'customers/record-dashboard.html', {'records': records})
+
+def customer_record_view(request, id):
+    record = get_object_or_404(Record, id=id)
+    return render(request, 'customers/record/individual-record.html', {'record': record})
+
+def add_record(request):
+	form = AddRecordForm(request.POST or None)
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			if form.is_valid():
+				form.save()
+				messages.success(request, "Record Added...")
+				return redirect(reverse('record-dashboard'))
+		return render(request, 'customers/add-record.html', {'form':form})
+
+
